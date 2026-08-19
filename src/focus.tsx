488e071bcbb7d4907f234397ff1023da3d4866ce
Umang1420 <umangchaudhari670@gmail.com>
2026-08-19 18:10:56 +0530

    Practise

--- src/focus.tsx
+++ src/focus.tsx
@@ -6,11 +6,21 @@ export default function Focus(){
         inputRef.current.focus()
     }
 
-    const countref = useRef<number>(0)
+    const countRef = useRef<number>(0)
 
     const handleCount = ()=>{
-       countref.current = countref.current + 1;
-        console.log(countref.current);
+       countRef.current = countRef.current + 1;
+        console.log(countRef.current);
+    }
+
+    const playRef = useRef<boolean>(false)
+
+    const handlePlay = ()=>{
+        if(playRef.current === true){
+            playRef.current = false;
+        }else{
+            playRef.current = true;
+        }
     }
 
     return (
@@ -18,6 +28,7 @@ export default function Focus(){
         <input type="text" ref={inputRef} />
         <button onClick={handleFocus}>Focus</button>
         <button onClick={handleCount}>Add</button>
+        <button onClick={handlePlay}>{playRef.current ? "Running" : "Start"}</button>
         </>
     )
 }
