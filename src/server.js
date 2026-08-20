fe4d45006bc455501d97df67c6ab5ec63a74ffc1
Umang1420 <umangchaudhari670@gmail.com>
2026-08-20 11:42:35 +0530

    Practice

--- src/server.js
+++ src/server.js
@@ -21,6 +21,20 @@ app.get('/user/:name', (req, res) => {
     res.json({ message: `Hello ${userName}` });
 });
 
+app.post('/', (req  , res) => {
+  res.send('Got a POST request');
+});
+app.put('/', (req  , res) => {
+  res.send('Got a PUT request');
+});
+app.get('/user', (req, res) => {
+    res.json({ message: "Please provide a username, like /user/John" });
+});
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user');
+});
+
 app.listen(port, () => {
     console.log(`Done, Server is running on http://localhost:${port}`);
 });
